5c916ba44338929472e94e1f75d871452469465d
fastestsunil <sunil141292@gmail.com>
2025-11-01 23:26:34 +0530

    Enhance layout and theming support by adding ThemeProvider to the Redux provider and integrating ThemeToggle in the SiteHeader. Updated RootLayout to suppress hydration warnings for improved rendering consistency.

--- src/redux/provider.tsx
+++ src/redux/provider.tsx
@@ -2,15 +2,21 @@
 
 import { Provider } from 'react-redux';
 import { ToastContainer } from 'react-toastify';
+import { ThemeProvider } from 'next-themes';
 import { store } from './store';
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <>
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="light"
+      enableSystem
+      disableTransitionOnChange
+    >
       <Provider store={store}>
         {children}
         <ToastContainer />
       </Provider>
-    </>
+    </ThemeProvider>
   );
 }
